c861e17e0a132f279bf583249f98f14d062c6798
Jcezar-js <julio.filho1@hotmail.com>
2021-09-08 16:05:47 -0300

    classes de modelo -2

---
+++ models/usuario.cs
@@ -0,0 +1,10 @@
+namespace musiC.Models
+{
+    public class Usuario{
+        public string username { get; set; }
+        public string email { get; set; }
+        public Biblioteca biblioteca { get; set; }
+
+        public amigos amigos { get; set; }
+    }
+}
