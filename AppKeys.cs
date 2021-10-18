1b34da46ad31405deff9808d86a3b262b8e147ae
Jcezar-js <julio.filho1@hotmail.com>
2021-10-18 13:40:49 -0300

    Credencials

--- AppKeys.cs
+++ AppKeys.cs
@@ -1,6 +1,6 @@
 namespace musiC
 {
     public static class AppKey{
-        public static string Key = "musiCmusiC";
+        public static string Key = "123456789123456789123456789912345"; //32 caracteres
     }
 }
---
+++ models/credencial.cs
@@ -0,0 +1,7 @@
+namespace musiC.Models { 
+	public class Credencial
+    {
+        public string Login { get; set; }
+        public string Senha { get; set; }
+    }
+}
